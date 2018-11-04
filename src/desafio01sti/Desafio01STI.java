1282b0588b1d90b20f5e9de2a473180217ea26dc
AndreAloise77 <44739910+AndreAloise77@users.noreply.github.com>
2018-11-04 06:15:59 -0200

    Update com todas as funcionalidades da classe.

--- src/desafio01sti/Desafio01STI.java
+++ src/desafio01sti/Desafio01STI.java
@@ -8,7 +8,6 @@ package desafio01sti;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
-
 /**
  *
  * @author André
@@ -68,6 +67,55 @@ public class Desafio01STI {
             if (aluno == null) {
                 System.out.println("A matrícula informada não existe.");
                 System.out.println("Por favor entre com um número de matrícula válido para realizar a operação!");
+            }
+        }
+        /**
+         * Esta parte consiste na verificação por 3 etapas: 1) Se após o usuário
+         * digitar a matricula corretamente o objeto aluno foi criado
+         * corretamente. 2) Se o aluno possui o status de Ativo ou Inativo 3) Se
+         * o aluno já possui ou não um uffMail.
+         */
+        if (aluno != null) {
+            //
+            if (aluno.getStatus().equals("Ativo")) {
+                //
+                if (aluno.getUffMail().equals("")) {
+                    System.out.println("");
+                    GeradorUffMail gerador = new GeradorUffMail(aluno);
+                    String pNome = gerador.getPrimeiroNome();
+                    System.out.println(pNome + ", por favor escolha uma das opções abaixo para o seu UFFMail");
+                    String[] sugestoes = gerador.opcoesUffMail();
+                    for (int i = 0; i < sugestoes.length; i++) {
+                        System.out.println((i + 1) + " - " + sugestoes[i]);
+                    }
+                    System.out.println("");
+                    int opcao = teclado.nextInt();
+                    System.out.println("");
+                    /**
+                     * Neste trecho é feita uma verificação para saber se o
+                     * usuário entrou com uma opção válida ou não. Caso ela não
+                     * seja válida, o sistema informa e pede uma nova entrada.
+                     */
+                    boolean escolhaErrada = true;
+                    while (escolhaErrada) {
+                        if (opcao < 1 || opcao > 5) {
+                            System.out.println("Opção Inválida, por favor escolha uma opção válida!");
+                            opcao = teclado.nextInt();
+                            System.out.println("");
+                        } else {
+                            System.out.println("A criação de seu e-mail (" + sugestoes[opcao - 1] + ") será feita nos próximos minutos.");
+                            String tel = aluno.getTelefone();
+                            System.out.println("Um SMS foi enviado para " + tel + " com a sua senha de acesso.");
+                            escolhaErrada = false;
+                        }
+                    }
+                } else {
+                    System.out.println("O aluno já possui um uffMail cadastrado.");
+                    System.out.println("infelizmente não é possível criar um novo para este aluno!");
+                }
+            } else {
+                System.out.println("No momento este aludo se encontra com o status de Inativo.");
+                System.out.println("Sendo assim não é possível realizar a criação de um uffMail para o mesmo!");
             }
         }
     }
